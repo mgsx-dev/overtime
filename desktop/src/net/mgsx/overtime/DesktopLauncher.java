eb24d97c2f483527c35ab723895c4765b1d3aeb9
Germain AUBERT <germain.aubert@gmail.com>
2018-10-27 04:04:29 +0200

    init from template

---
+++ desktop/src/net/mgsx/overtime/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mgsx.overtime;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mgsx.overtime.OverTime;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OverTime(), config);
+	}
+}
